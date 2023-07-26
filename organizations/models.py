96ea7cdf7fb4ff5f59007689c7a3e27c00ebc9b9
AlexShmigelskii <s5003626@gmail.com>
2023-07-26 20:48:12 +0300

    commit 3

--- organizations/models.py
+++ organizations/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.postgres.indexes import GinIndex
 from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.fields import ArrayField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -17,6 +18,9 @@ class Organization(models.Model):
 
     # ИНН (Идентификационный номер налогоплательщика) организации (максимальная длина - 12 символов).
     inn = models.CharField(max_length=12)
+
+    # Поле для хранения списка стоп-слов.
+    stop_words = ArrayField(models.CharField(max_length=100), default=list, blank=True)
 
     search_vector = models.TextField(null=True, blank=True)
 
@@ -38,5 +42,6 @@ class Organization(models.Model):
 # Создаем обработчик сигнала для post_save
 @receiver(post_save, sender=Organization)
 def update_search_vector(sender, instance, **kwargs):
-    instance.search_vector = SearchVector('full_name', 'short_name')
+    # Обновляем поле search_vector с использованием SearchVector и исключаем стоп-слова.
+    instance.search_vector = SearchVector('full_name', 'short_name', config='russian') - SearchVector(*instance.stop_words, config='russian')
     instance.save()
